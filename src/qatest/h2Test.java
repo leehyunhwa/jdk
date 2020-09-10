b5738034955f431412677effad9036628c0f107b
leehyunhwa <hyunhwa_lee@tmax.co.kr>
2020-09-10 01:45:38 +0000

    aaaaaaaaaaaaaaaa

--- src/qatest/h2Test.java
+++ src/qatest/h2Test.java
@@ -2,7 +2,10 @@ public class h2Test {
 
 	public static void main(String[] args) {
 		
-        System.out.println("QATEST ADD 20200910"); 
+        System.out.println("QATESTQAQAQAQA"); 
+        System.out.println("QATESTQAQAQAQA"); 
+        System.out.println("QATESTQAQAQAQA"); 
+        System.out.println("QATESTQAQAQAQA"); 
 	}
 
 }
--- src/qatest/test.java
+++ src/qatest/test.java
@@ -3,6 +3,9 @@ public class test {
 	public static void main(String[] args) {
 		
         System.out.println("QATESTQAQAQAQA"); 
+        System.out.println("QATESTQAQAQAQA"); 
+        System.out.println("QATESTQAQAQAQA"); 
+        System.out.println("QATESTQAQAQAQA"); 
 	}
 
 }
